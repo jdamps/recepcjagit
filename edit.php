30160676e8b7678c780eb8253b23b080437f13ab
jdamps <joanna.damps@gmail.com>
2019-09-07 19:34:31 +0200

    działająca strona z formularzem edit

---
+++ edit.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title> Edycja - PRACOWNIK </title>
+</head>
+<body>
+<?php
+
+$con=mysqli_connect("localhost","jdamps","admin123","recepcja1");
+
+$sql = "SELECT * FROM pracownicy WHERE id_pracownik='$_GET[id]'";
+
+$records = mysqli_query($con,$sql);
+
+?>
+
+<table width='600' border='1' cellpadding='1' cellspacing='1'>
+
+	<tr>
+	<th>Login</th>
+	<th>Imię</th>
+	<th>Nazwisko</th>
+	<th>Telefon</th>
+	<th>Opis</th>
+	</tr>
+	
+	<?php
+	while($row = mysqli_fetch_array($records))
+	{
+		echo "<tr><form method=POST action=update.php>";
+		echo "<td><input type=text name=login_pracownik value='".$row['login_pracownik']."'></td>";
+		echo "<td><input type=text name=imie_pracownik value='".$row['imie_pracownik']."'></td>";
+		echo "<td><input type=text name=nazwisko_pracownik value='".$row['nazwisko_pracownik']."'></td>";
+		echo "<td><input type=text name=tel_pracownikk value='".$row['tel_pracownik']."'></td>";
+		echo "<td><input type=text name=opis_pracownik value='".$row['opis_pracownik']."'></td>";
+		echo "<td><button type=submit name=submit >Zmień</button></th>";
+		echo "</form></tr>";
+		
+	}
+	
+	
+	
+	?>
